77a84adebcdda746536e654733b2488d204b2ad9
Apresh Agarwal <apreshagrwl12@gmail.com>
2020-09-08 16:24:44 +0530

    Updated DB credentials

--- connection.php
+++ connection.php
@@ -2,4 +2,4 @@
 define('EXAM_KEY_PREFIX', 'prismCodeCertificationSystem', true);
 define('GOOGLE_RECAPTCHA_PUB', '***REMOVED***', true);
 define('GOOGLE_RECAPTCHA_SEC', '***REMOVED***', true);
-$conn = new mysqli('localhost', 'root', '', 'certification_system');
+$conn = new mysqli('***REMOVED***', '***REMOVED***', '***REMOVED***', '***REMOVED***');
